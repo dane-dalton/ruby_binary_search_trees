5d9083c0eef68a5f591d9e05a30f59e614c0d8d8
Dane Dalton <danezdalton1@gmail.com>
2022-07-20 10:14:46 -0500

    Generate a random array of elements to build a tree, and rough draft for #build_tree

--- lib/main.rb
+++ lib/main.rb
@@ -1,17 +1,50 @@
 class Node
-  attr_accessor :value, :left, :right
+  attr_accessor :data, :left, :right
 
-  def initialize(value = nil)
-    @value = value
+  def initialize(data)
+    @data = data
     @left = nil
     @right = nil
   end
 end
 
-class BinarySearchTree
-  attr_accessor :root
+class Tree
+  attr_accessor :root, :rand_arr
 
   def initialize()
-    @root = nil
+    @rand_arr = random_array()
+    @root = self.build_tree(rand_arr, 0, rand_arr.length - 1)
+  end
+
+  def random_array()
+    array = []
+    10.times do
+      value = rand(100)
+      # For simplicities sake, don't use duplicates atm - makes balancing the tree more difficult
+      array << value unless array.any?(value)
+    end
+    return array
+  end
+
+  def build_tree(array, start, end_element)
+    return nil if start > end_element
+    mid = (end_element - start) / 2
+    @root = Node.new(array[mid]) if @root == nil
+    temp = @root
+    p temp
+    p array
+    temp.left = build_tree(array, start, mid - 1)
+    temp.right = build_tree(array, mid + 1, end_element)
+
+    return Node.new(array[mid])
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+tree = Tree.new()
+tree.pretty_print
